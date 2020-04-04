1b031bc2bb8f20c10b71155fbc1ea831a7283745
droll-window-9768 <ppirotte@gmail.com>
2020-04-04 19:52:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,7 +2,7 @@ function basicTeenager(age) {
   if (13 <= age && age < 20)
   return("You are a teenager!")
 } else {
-  "You are not a teenager!"
+  return("You are not a teenager!")
 }
 
 function teenager(age) {
